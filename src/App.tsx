14ec7b667c4daad92b12045863fc7ebf432c3d2f
Sivak Hico <strukggg@gmail.com>
2023-02-12 19:36:23 +0100

    map geted already

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-
+import React from 'react'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import './App.css'
 
-function App() {
-
+export default function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   )
 }
-
-export default App
